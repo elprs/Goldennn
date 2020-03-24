82e504fa5564b58b1778ee6564018f60107a235a
elprs <eleni.parisi.sunshine@gmail.com>
2020-03-24 20:36:09 +0200

    class App

--- Goldenn.Desktop/Program.cs
+++ Goldenn.Desktop/Program.cs
@@ -6,27 +6,51 @@ using System.Text;
 using System.Threading.Tasks;
 using Goldennn.Database;
 using Goldennn.Entities;
+using Goldennn.Services;
 
 namespace Goldenn.Desktop
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
             using(MyDatabase db = new MyDatabase())
             {
-                //var movies = db.Movies; //san I quirable interfase // tha steilei SQL QUERY STI BASI 
+                //var movies = db.Movies; //san I quirable interfase // tha steilei SQL QUERY STI BASI //zitao ka8e fora apo toservar na mou stelnei thn katallili pliroforia // de me endiaferei gia ena sugkekrimeno query sti basi kai exo para polu RAM 
+                
 
                 var movies = db.Movies.ToList(); //simbato me IEnumerable //tha stalei query sti basi poy 8a sou ferei ol aosa xxreiazomai sato run time
+                //tha to xrismopoiiso gia polla//tha ferei osa xreiazomai sto run time
+                //fere mou AUTOO apo ti basi, stoxeuMENA!!!!! me linq 
 
                 foreach (var movie in movies)
                 {
                     Console.WriteLine("{0,-6}{0,-15}", movie.MovieId, movie.Title);
                 }
             }
-
+          
 
             Console.ReadKey();
         }
+
+        public  class App
+        {
+            public  static void PrintMovie()
+            {
+                Goldennn.Services.MovieRepository movieRepository = new MovieRepository();
+                var movies = movieRepository.GetAll().Where(x => x.Title.Contains("e"));
+                //auti ti stigmi i linq psaxnei sto run, momeory (oxi ston sql server, auto einai allos dromos). 
+
+
+                foreach (var movie in movies)
+                {
+                    Console.WriteLine("{0,-6}{0,-15}", movie.MovieId, movie.Title);
+                }
+            }
+        }
+
+        
+        
     }
 }
+
---
+++ Goldennn.Services/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Goldennn.Database;
+using Goldennn.Entities;
+
+namespace Goldennn.Services
+{
+
+    //services
+    //dokimasa na paro ola ta prahmata se lidta. ta pira ola. Get all loipon. 
+    //GetAll // de 8a sundeetai me to datadata base alla 8a ta pairnei apo to service
+    //periame//leitoyrgo//anagnotisa//toxw
+    //erxomai sto sevices kai tha ftiakso mia klass pou tja tin po   MovieRepository
+    public class MovieRepository //giati o apo8ikarios fernei apo to repositoty tainiew. service apo8ikis
+
+    {
+        public IEnumerable<Movie> GetAll()
+            {
+        using (MyDatabase db = new MyDatabase())
+        {
+                return db.Movies.ToList();
+        }
+    }
+}
+}
